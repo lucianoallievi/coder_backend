f8784db2fedb521c999ac12aaf2b42bfab1743d7
Luciano Allievi <lucianoallievi@gmail.com>
2023-12-12 22:52:51 -0300

    firt commit

---
+++ productManager.js
@@ -0,0 +1,57 @@
+class ProductManager {
+  static #products = [];
+
+  // Cada producto tiene las propiedades:
+  // - id (código identificador)
+  // - title (titulo)
+  // - photo (ruta de imagen)
+  // - price (precio)
+  // - stock (unidades disponibles)
+
+  create(data) {
+    if (!(data.title && data.photo && data.price && data.stock))
+      return console.log(
+        "Datos faltantes. Obligatorios: title, photo, price, stock. Enviados:",
+        data
+      );
+    const product = {
+      id:
+        ProductManager.#products.length === 0
+          ? 1
+          : ProductManager.#products[ProductManager.#products.length - 1].id +
+            1,
+      title: data.title,
+      photo: data.photo,
+      price: data.price,
+      stock: data.stock,
+    };
+
+    ProductManager.#products.push(product);
+  }
+
+  read() {
+    return ProductManager.#products;
+  }
+
+  readOne(id) {
+    return ProductManager.#products.find((one) => one.id == id);
+  }
+}
+
+const pm = new ProductManager();
+
+pm.create({
+  title: "medias",
+  photo: "photo.jpg",
+  price: 2.44,
+  stock: 90,
+});
+
+pm.create({
+  title: "zapatillas",
+  photo: "photo.jpg",
+  //price: 20.5,
+  stock: 50,
+});
+
+console.log(pm.readOne(1));
---
+++ userManager.js
@@ -0,0 +1,52 @@
+class UserManager {
+  static #users = [];
+
+  // Cada usuario tiene las propiedades:
+  // - id (código identificador)
+  // - name (titulo)
+  // - photo (ruta de imagen)
+  // - email (precio)
+
+  create(data) {
+    if (!(data.name && data.photo && data.email))
+      return console.log(
+        "Datos faltantes. Obligatorios: name, photo, email. Enviados:",
+        data
+      );
+    const user = {
+      id:
+        UserManager.#users.length === 0
+          ? 1
+          : UserManager.#users[UserManager.#users.length - 1].id + 1,
+      name: data.name,
+      photo: data.photo,
+      email: data.email,
+    };
+
+    UserManager.#users.push(user);
+  }
+
+  read() {
+    return UserManager.#users;
+  }
+
+  readOne(id) {
+    return UserManager.#users.find((one) => one.id == id);
+  }
+}
+
+const um = new UserManager();
+
+um.create({
+  name: "carlos",
+  photo: "carlos.jpg",
+  email: "carlos@dominio.com",
+});
+
+um.create({
+  name: "roberto",
+  //photo: "roberto.jpg",
+  email: "roberto@dominio.com",
+});
+
+console.log(um.readOne(1));
